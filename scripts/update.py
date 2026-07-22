253f3d00cab936b75bd1a28e1d4defa1d197e740
satinfo50 <satinfo50@users.noreply.github.com>
2026-07-22 11:45:25 +0330

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -3,7 +3,7 @@ import json
 import urllib.request
 
 
-API = "https://stat.ripe.net/data/country-resource-list/data.json?resource=ir&v4_format=prefix"
+API = "https://stat.ripe.net/data/country-resource-list/data.json?resource=IR"
 
 
 os.makedirs("data", exist_ok=True)
@@ -14,10 +14,28 @@ print("Downloading Iran IP list...")
 
 
 with urllib.request.urlopen(API) as response:
-    data = json.loads(response.read())
+    raw = response.read()
 
 
-ips = data["resources"]["ipv4"]
+# ذخیره پاسخ خام برای بررسی
+with open("data/ripe-response.json", "wb") as f:
+    f.write(raw)
+
+
+data = json.loads(raw)
+
+
+print("JSON keys:")
+print(data.keys())
+
+
+if "resources" not in data:
+    print("No resources key found")
+    print(json.dumps(data, indent=2)[:2000])
+    exit(1)
+
+
+ips = data["resources"].get("ipv4", [])
 
 
 print("IPv4 count:", len(ips))
@@ -28,13 +46,11 @@ with open("data/current.txt", "w") as f:
         f.write(ip + "\n")
 
 
-
 with open("output/iran-full.rsc", "w") as f:
 
     f.write("/ip firewall address-list\n")
 
     for ip in ips:
-
         f.write(
             'add list=Iran address='
             + ip
@@ -42,4 +58,4 @@ with open("output/iran-full.rsc", "w") as f:
         )
 
 
-print("iran-full.rsc created")
+print("Done")
