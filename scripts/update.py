b5d53fcd96b521b0357304099c4b0e52df736f0f
satinfo50 <satinfo50@users.noreply.github.com>
2026-07-22 11:25:06 +0330

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,79 +1,46 @@
 import os
 import urllib.request
-import difflib
 import urllib.error
 
 
-URL = "https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/TXT/IR/IPv4.txt"
+URL = "https://raw.githubusercontent.com/HotCakeX/Official-IANA-IP-blocks/main/IR/IPv4.txt"
+
 
 os.makedirs("data", exist_ok=True)
 os.makedirs("output", exist_ok=True)
 
-
-old_file = "data/previous.txt"
 new_file = "data/current.txt"
 
-
-# دانلود لیست جدید
-
-print("Downloading...")
+print("Downloading:")
 print(URL)
 
-urllib.request.urlretrieve(
-    URL,
-    new_file
-)
+try:
+    urllib.request.urlretrieve(
+        URL,
+        new_file
+    )
 
-print("Downloaded successfully")
-
-
-# خواندن لیست قبلی
-
-old_ips = []
-
-if os.path.exists(old_file):
-    with open(old_file) as f:
-        old_ips = [
-            x.strip()
-            for x in f.readlines()
-            if x.strip()
-        ]
+except urllib.error.HTTPError as e:
+    print("Download failed:", e)
+    exit(1)
 
 
-# خواندن لیست جدید
+print("Download OK")
+
 
 with open(new_file) as f:
-    new_ips = [
+    ips = [
         x.strip()
         for x in f.readlines()
         if x.strip()
     ]
 
 
-old_set = set(old_ips)
-new_set = set(new_ips)
-
-
-added = sorted(new_set - old_set)
-removed = sorted(old_set - new_set)
-
-
-
-print("Added:", len(added))
-print("Removed:", len(removed))
-
-
-
-# ساخت فایل کامل
-
-with open(
-    "output/iran-full.rsc",
-    "w"
-) as f:
+with open("output/iran-full.rsc", "w") as f:
 
     f.write("/ip firewall address-list\n")
 
-    for ip in new_ips:
+    for ip in ips:
 
         f.write(
             'add list=Iran address='
@@ -82,47 +49,4 @@ with open(
         )
 
 
-
-# ساخت فایل تغییرات
-
-with open(
-    "output/iran-update.rsc",
-    "w"
-) as f:
-
-
-    f.write("/ip firewall address-list\n\n")
-
-
-    for ip in removed:
-
-        f.write(
-            ':do {remove [find list=Iran address="'
-            + ip
-            + '"]} on-error={}\n'
-        )
-
-
-    for ip in added:
-
-        f.write(
-            'add list=Iran address='
-            + ip
-            + ' comment="AUTO-IANA"\n'
-        )
-
-
-
-# ذخیره نسخه جدید برای فردا
-
-with open(
-    old_file,
-    "w"
-) as f:
-
-    for ip in new_ips:
-        f.write(ip+"\n")
-
-
-
-print("Done")
+print("RSC generated:", len(ips))
